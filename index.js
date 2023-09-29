4e7dce0a5bc3d251a6e36fefb3110257fc94d437
HarshaSandaruvan <133190578+HarshaSandaruvan@users.noreply.github.com>
2023-09-29 23:10:34 +0530

    Server is connected

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+import express, { json } from "express";
+import morgan from "morgan";
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(json());
+app.use(morgan("dev"));
+
+let posts =[];
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+});
