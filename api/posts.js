e771a76497057d2f315c50c978b8fa6aef85bc92
HarshaSandaruvan <133190578+HarshaSandaruvan@users.noreply.github.com>
2023-09-29 23:47:29 +0530

    delete method is create

--- api/posts.js
+++ api/posts.js
@@ -1,26 +1,36 @@
 import { Router } from "express";
 const postRouter = Router();
 
-let posts =[];
+let posts = [];
 
 postRouter.route("/").post((req, res) => {
     const body = req.body;
     posts.push(body);
     res.json(body).status(201);
-}).get((req,res)=>{
+}).get((req, res) => {
     res.json(posts);
 });
 
 postRouter.route("/:id").get((req, res) => {
-    const postID=req.params.id;
+    const postID = req.params.id;
     console.log(postID);
-    const post=posts.find((post)=>post.id===postID);
+    const post = posts.find((post) => post.id === postID);
 
     if (post) {
         res.json(post);
-    }else{
-        res.status(404).json({message:"Post not found !"})
+    } else {
+        res.status(404).json({ message: "Post not found !" })
     }
+}).delete((req, res) => {
+    const postID = req.params.id;
+    const post = posts.findIndex((post) => post.id === postID);
+    if(post!==-1){
+        posts.splice(post,1);
+        res.json({message: "Post Deleted !"})
+    }else {
+        res.status(404).json({ message: "Post not found !" })
+    }
+
 });
 
 export default postRouter;
