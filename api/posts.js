67e3700c1876c9527f5eec893e708b363eb7cb98
HarshaSandaruvan <133190578+HarshaSandaruvan@users.noreply.github.com>
2023-09-29 23:36:05 +0530

    get all method is create

--- api/posts.js
+++ api/posts.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 const postRouter = Router();
 
 let posts =[];
+
 postRouter.route("/").post((req, res) => {
     const body = req.body;
     posts.push(body);
     res.json(body).status(201);
+}).get((req,res)=>{
+    res.json(posts);
 });
 
 export default postRouter;
